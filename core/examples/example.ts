2d5ea4b4dcc86022281eeab417153eb22224c3cb
realhunion <hunainali099@gmail.com>
2023-05-04 15:41:32 -0500

    ESLint Fixes

--- core/examples/example.ts
+++ core/examples/example.ts
@@ -3,17 +3,23 @@ import { Soul, OpenaiConfig, OpenaiModel, Personality, Personalities } from "../
 
 const config = new OpenaiConfig({ apiKey: "...", model: OpenaiModel.gpt_3_5_turbo });
 
-
 let personality = Personalities.Samantha;
-personality = new Personality("...", "...", "...", "...")
+personality = Personalities.Dustin
+personality = Personalities.Page
+personality = Personalities.Reggie
+personality = Personalities.Eileen
+
+personality = { name: "...", shortPersonality: "...", longPersonality: "...", initialPlan: "..." }
+
+personality = Personalities.Samantha
 
 const soul = new Soul(personality);
 
-soul.on("says", (text : String) => {
+soul.on("says", (text: String) => {
   console.log("👱", personality.name, " says: ", text);
 });
 
-soul.on("thinks", (text : String) => {
+soul.on("thinks", (text: String) => {
   console.log("👱", personality.name, " thinks: ", text);
 });
 
@@ -25,15 +31,15 @@ const rl = readline.createInterface({
 console.log('- Type a message to send to Soul\n- Type "reset" to reset\n- Type "exit" to quit\n');
 
 rl.on("line", async (line) => {
-    if (line.toLowerCase() === "exit") {
-        rl.close();
-    }
-    else if (line.toLowerCase() === "reset") {
-        soul.reset();
-    }
-    else {
-        const text : string = line;
-        soul.tell(text);
+  if (line.toLowerCase() === "exit") {
+    rl.close();
+  }
+  else if (line.toLowerCase() === "reset") {
+    soul.reset();
+  }
+  else {
+    const text: string = line;
+    soul.tell(text);
   }
 });
 
--- core/src/index.ts
+++ core/src/index.ts
@@ -2,9 +2,7 @@ import { OpenaiConfig, OpenaiModel } from "./gptStream";
 export { OpenaiConfig, OpenaiModel };
 
 import { Soul } from "./soul";
-export { Soul }
+export { Soul };
 
 import { Personality, Personalities } from "./personality";
-export { Personality, Personalities }
-
-
+export { Personality, Personalities };
