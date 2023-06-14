2925ca80cfb026f015c469c1d926c0112c77dafb
kevinfischer <fischer.kevin.a@gmail.com>
2023-06-14 00:08:42 -0400

    second pass - starting to work

--- core/src/lmProgram.ts
+++ core/src/lmProgram.ts
@@ -4,13 +4,14 @@ import {
   Configuration,
   OpenAIApi,
 } from "openai";
-import { processLMProgram } from "./lmProcessing";
 import { devLog } from "./utils";
 import { OpenAIExt } from "openai-ext";
-import { Thought } from "./lmStream";
-import EventEmitter from "events";
 
-const dumy = `
+/*
+
+The lmYield parses an lmProgram, all its features are shown below.
+
+const lmYield = lmYield(`
 {{#context~}}
 {{! this block compiles to system in oai}}
 
@@ -39,18 +40,13 @@ const dumy = `
 <FEELS>I feel {{gen 'feeling' until '</FEELS>'}}
 ...
 {{~/yield}}
-`;
+`)
 
-// class lmProgram {
-//   private program: string;
-//   constructor(program: string) {
-//     this.program = program.trim();
-//   }
-//
-//   private programToGPTChat(): ChatCompletionRequestMessage[] {
-//     this.program;
-//   }
-// }
+ */
+
+const apiKey = process.env.OPENAI_API_KEY;
+const configuration = new Configuration({ apiKey });
+const openaiApi = new OpenAIApi(configuration);
 
 type StringReplacements = {
   [key: string]: string;
@@ -120,7 +116,7 @@ enum blockTypes {
 
 type OAIProgram = ChatCompletionRequestMessage[];
 
-function compileToOAI(blocks: Block[], generationProgress = ""): OAIProgram {
+function compileToOAI(blocks: Block[]): OAIProgram {
   if (blocks.slice(-1)[0].blockName !== "yield") {
     throw new Error("yield block must be last");
   }
@@ -142,79 +138,8 @@ function compileToOAI(blocks: Block[], generationProgress = ""): OAIProgram {
       }
       return message;
     });
-  if (generationProgress.length > 0) {
-    // magic
-  }
   return messages;
 }
-
-const program = `
-{{#context~}}
-The following is a conversation between <Bogus>{{personality}}</Bogus>, and an unknown person.
-
-Several thought patterns are references through the conversation:
-
-Bogus uses the following <INTERNAL_DIALOG/> to think through what it says next.
-
-<INTERNAL_DIALOG>
-  <FELT>Bogus felt ...</FELT>
-  <THOUGHT>Bogus thought ...</THOUGHT>
-  <SAID>Bogus said "..."</SAID>
-  <ANALYZED>Next, I plan to ...</ANALYZED>
-</INTERNAL_DIALOG>
-<END />
-{{~/context}}
-
-{{#human~ name='xyz'}}
-<MEMORY for="xyz">
-  <SAID>"Hi"</SAID>
-</MEMORY>
-{{~/human}}
-
-{{#generated~}}
-<MEMORY for="Bogus">
-  <FELT>Bogus felt excited and hungry</FELT>
-  <THOUGHT>Bogus thought perhaps another victim for me to feast upon</THOUGHT>
-  <SAID>Bogus said "Hello there, little one. What brings you to my neck of the woods?"</SAID>
-  <ANALYZED>Next, Bogus planed to lull them into a false sense of security before pouncing.</ANALYZED>
-</MEMORY>
-{{~/generated}}
-
-{{#human~ name='xyz'}}
-<MEMORY for="xyz">
-  <SAYS>"I think I'm lost"</SAYS>
-</MEMORY>
-{{~/human}}
-
-{{#instructions~}}
-Then, Bogus had the following <INTERNAL_DIALOG />
-{{~/instructions}}
-
-{{#yield~}}
-<INTERNAL_DIALOG>
-  <FELT>Bogus felt {{gen 'feeling' until '</FELT>'}}
-  <THOUGHT>Bogus thought {{gen 'thought' until '</THOUGHT>'}}
-  <SAID>Bogus said "{{gen 'saying' until '"</SAID>'}}
-  <ANALYZED>Next, Bogus plans to {{gen 'analyzed' until '</ANALYZED>'}}
-</INTERNAL_DIALOG>
-<END />
-{{~/yield}}
-`;
-//<FEELS>I feel {{gen 'feeling' until '</FEELS>'}}
-
-const apiKey = process.env.OPENAI_API_KEY;
-const configuration = new Configuration({ apiKey });
-const openaiApi = new OpenAIApi(configuration);
-
-const blocks = parseProgram(program, [
-  {
-    personality: "Bogus, an evil witch that eats children",
-  },
-]);
-if (blocks.length < 2) {
-  throw new Error("Missing blocks in lmProgram");
-}
-const oaiProgram = compileToOAI(blocks);
 
 type YieldInstruction = {
   varName: string;
@@ -253,8 +178,6 @@ function parseYieldBlock(yieldContent: string): YieldInstruction[] {
   }
   return results;
 }
-const yieldBlock = blocks.slice(-1)[0];
-const yieldInstructions = parseYieldBlock(yieldBlock.content);
 
 type Yield = {
   name: string;
@@ -262,80 +185,115 @@ type Yield = {
   instruction: YieldInstruction;
 };
 
-type onPartialGenType = (partial: string) => void;
+function delay(milliseconds: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, milliseconds);
+  });
+}
+
+type OAIStream = any;
 
 class LMYield {
   public oaiProgram: OAIProgram;
   public yieldInstructions: YieldInstruction[];
   public yields: Yield[] = [];
+  public yieldComplete = false;
 
-  constructor(oaiProgram: OAIProgram, yieldInstructions: YieldInstruction[]) {
-    this.oaiProgram = oaiProgram;
-    this.yieldInstructions = yieldInstructions;
+  constructor(program: string) {
+    const blocks = parseProgram(program, [
+      {
+        personality: "Bogus, an evil witch that eats children",
+      },
+    ]);
+    if (blocks.length < 2) {
+      throw new Error("Missing blocks in lmProgram");
+    }
+    const yieldBlock = blocks.slice(-1)[0];
+    this.yieldInstructions = parseYieldBlock(yieldBlock.content);
+    this.oaiProgram = compileToOAI(blocks);
   }
 
   public async generate() {
     await this.onPartialGen();
-    while (yieldInstructions.length > 0) {
-      // wait
+    while (!this.yieldComplete) {
+      await delay(10);
     }
     return this.yields;
   }
 
-  private async onPartialGen(partial = "") {
+  private async onPartialGen(
+    partial = "",
+    nextYieldInstruction: YieldInstruction | null = null
+  ) {
     const program = this.addPartialToOAIProgram(partial);
-    await this.yield(program, this.onPartialGen);
+    await this.yield(program, nextYieldInstruction);
   }
 
   private addPartialToOAIProgram(partialGen: string) {
     const lastElement = this.oaiProgram.slice(-1)[0];
-    return oaiProgram.slice(0, -1).concat({
+    const additionalFormatting =
+      partialGen.length > 0 ? "\n\n" + partialGen : "";
+    return this.oaiProgram.slice(0, -1).concat({
       ...lastElement,
-      content: lastElement.content + "\n\n" + partialGen,
+      content: lastElement.content + additionalFormatting,
     });
   }
 
-  private async yield(program: OAIProgram, onPartialGen: onPartialGenType) {
-    yieldInstructions.reverse();
-    if (yieldInstructions.length === 0) {
+  private async yield(
+    program: OAIProgram,
+    nextYieldInstruction: YieldInstruction | null
+  ) {
+    console.log("NEW STREAM WITH", nextYieldInstruction, program.slice(-1)[0]);
+    this.yieldInstructions.reverse();
+    if (this.yieldInstructions.length === 0 && nextYieldInstruction === null) {
       throw new Error("Yield instruction block missing any gen instructions");
     }
-    let currentYieldInstruction = yieldInstructions.pop() as YieldInstruction;
+    let currentYieldInstruction =
+      nextYieldInstruction ||
+      (this.yieldInstructions.pop() as YieldInstruction);
     let parsedGeneration = "";
     let generation = "";
-    let stream: any;
-    const restart = (partialGen: string) => {
-      console.log("NEED TO RESTART\n\n", partialGen);
-      const nextPrompting = currentYieldInstruction.priorChunk;
-      const gen = this.yields
-        .map(
-          (y) => y.instruction.priorChunk + y.value + y.instruction.closureTag
-        )
-        .concat([nextPrompting])
-        .join("\n");
-      onPartialGen(gen);
+    let restarting = nextYieldInstruction !== null;
+    let stream: OAIStream;
+    const restart = () => {
+      if (stream) {
+        stream.destroy();
+        console.log("Decoding deviation, restarting");
+        const nextPrompting = currentYieldInstruction.priorChunk;
+        const gen = this.yields
+          .map(
+            (y) => y.instruction.priorChunk + y.value + y.instruction.closureTag
+          )
+          .concat([nextPrompting])
+          .join("\n");
+        this.onPartialGen(gen, currentYieldInstruction);
+      }
     };
     const openaiStreamConfig = {
       openai: openaiApi,
       handler: {
         onContent: (content: string) => {
-          console.log(content);
           generation = content.trimStart();
           const partialGen = generation.slice(parsedGeneration.length);
           if (
             partialGen.startsWith(
               currentYieldInstruction.priorChunk.slice(0, partialGen.length)
             ) &&
-            partialGen.length < currentYieldInstruction.priorChunk.length
+            partialGen.length < currentYieldInstruction.priorChunk.length &&
+            !restarting
           ) {
             // currently valid generation
             return;
           } else if (
-            partialGen.startsWith(currentYieldInstruction.priorChunk) &&
-            partialGen.length >= currentYieldInstruction.priorChunk.length
+            (partialGen.startsWith(currentYieldInstruction.priorChunk) &&
+              partialGen.length >= currentYieldInstruction.priorChunk.length) ||
+            restarting
           ) {
             // currently generating a variable
-            if (partialGen.startsWith(currentYieldInstruction.priorChunk)) {
+            if (
+              partialGen.startsWith(currentYieldInstruction.priorChunk) ||
+              restarting
+            ) {
               // check for yield instruction closure
               const remainingGen = partialGen.slice(
                 currentYieldInstruction.priorChunk.length
@@ -354,12 +312,13 @@ class LMYield {
                     value,
                     instruction: currentYieldInstruction,
                   });
-                  if (yieldInstructions.length > 0) {
+                  if (this.yieldInstructions.length > 0) {
                     currentYieldInstruction =
-                      yieldInstructions.pop() as YieldInstruction;
+                      this.yieldInstructions.pop() as YieldInstruction;
+                    restarting = false;
                     parsedGeneration = generation;
                   } else {
-                    stream.abort();
+                    stream?.abort();
                   }
                 } else {
                   throw new Error("NEED TO FIX THIS ERROR 108");
@@ -368,27 +327,21 @@ class LMYield {
                 // todo - add some streaming here
               }
             } else {
-              console.log("a", {
-                generation,
-                chunk: currentYieldInstruction.priorChunk,
-              });
-              restart(generation);
+              console.log("a");
+              restart();
             }
           } else {
-            console.log("b", {
-              yields: this.yields,
-              generation,
-              partialGen,
-              currentYieldInstruction,
-            });
-            restart(generation);
+            console.log("b");
+            restart();
           }
         },
         onDone: () => {
-          if (yieldInstructions.length > 0) {
-            restart(generation);
+          if (this.yieldInstructions.length > 0) {
+            console.log("c");
+            restart();
+          } else {
+            this.yieldComplete = true;
           }
-          console.log("WTFFFFF", this.yields);
         },
       },
     };
@@ -404,9 +357,63 @@ class LMYield {
       openaiStreamConfig
     );
 
-    console.log("STREAMING");
     stream = openaiStreamResponse.data;
   }
 }
-const lmYield = new LMYield(oaiProgram, yieldInstructions);
-lmYield.generate();
+
+// example
+
+const lmYield = new LMYield(`
+{{#context~}}
+The following is a conversation between <Bogus>{{personality}}</Bogus>, and an unknown person.
+
+Several thought patterns are references through the conversation:
+
+Bogus uses the following <INTERNAL_DIALOG/> to think through what it says next.
+
+<INTERNAL_DIALOG>
+  <FELT>Bogus felt ...</FELT>
+  <THOUGHT>Bogus thought ...</THOUGHT>
+  <SAID>Bogus said "..."</SAID>
+  <ANALYZED>Next, I plan to ...</ANALYZED>
+</INTERNAL_DIALOG>
+<END />
+{{~/context}}
+
+{{#human~ name='unknown'}}
+<MEMORY for="unknown">
+  <SAID>"Hi"</SAID>
+</MEMORY>
+{{~/human}}
+
+{{#generated~}}
+<MEMORY for="Bogus">
+  <FELT>Bogus felt excited and hungry</FELT>
+  <THOUGHT>Bogus thought perhaps another victim for me to feast upon</THOUGHT>
+  <SAID>Bogus said "Hello there, little one. What brings you to my neck of the woods?"</SAID>
+  <ANALYZED>Next, Bogus planed to lull them into a false sense of security before pouncing.</ANALYZED>
+</MEMORY>
+{{~/generated}}
+
+{{#human~ name='xyz'}}
+<MEMORY for="xyz">
+  <SAYS>"I think I'm lost"</SAYS>
+</MEMORY>
+{{~/human}}
+
+{{#instructions~}}
+Then, Bogus had the following <INTERNAL_DIALOG />
+{{~/instructions}}
+
+{{#yield~}}
+<INTERNAL_DIALOG>
+  <FELT>Bogus felt {{gen 'feeling' until '</FELT>'}}
+  <THOUGHT>Bogus thought {{gen 'thought' until '</THOUGHT>'}}
+  <SAID>Bogus said "{{gen 'saying' until '"</SAID>'}}
+  <ANALYZED>Next, Bogus plans to {{gen 'analyzed' until '</ANALYZED>'}}
+</INTERNAL_DIALOG>
+<END />
+{{~/yield}}
+`);
+
+lmYield.generate().then((yields) => console.log(yields));
