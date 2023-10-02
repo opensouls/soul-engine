238bacfa54b1e65c9b6e99aeb55b819e15e70f13
Topper Bowers <topper@toppingdesign.com>
2023-10-02 21:58:54 +0200

    remove commented out code

--- core/src/languageModels/index.ts
+++ core/src/languageModels/index.ts
@@ -129,41 +129,6 @@ export interface StreamingChatCompletionEventChoices {
   index?: number;
 }
 
-type StreamingChatCompletionEvent = AsyncIterable<ChatCompletionChunk>
-
-// export interface StreamingChatCompletionEvent {
-//   choices: StreamingChatCompletionEventChoices[];
-
-//   created: number;
-
-//   id: string;
-
-//   model: string;
-
-//   object: string;
-// }
-
-// export interface FunctionSpecification {
-//   /**
-//    * The name of the function to be called. Must be a-z, A-Z, 0-9, or contain
-//    * underscores and dashes, with a maximum length of 64.
-//    */
-//   name: string;
-
-//   /**
-//    * The description of what the function does.
-//    */
-//   description?: string;
-
-//   /**
-//    * The parameters the functions accepts, described as a JSON Schema object. See the
-//    * [guide](/docs/guides/gpt/function-calling) for examples, and the
-//    * [JSON Schema reference](https://json-schema.org/understanding-json-schema/) for
-//    * documentation about the format.
-//    */
-//   parameters?: Record<string, unknown>;
-// }
-
 export interface CreateChatCompletionParams {
   /**
    * A list of messages comprising the conversation so far.
