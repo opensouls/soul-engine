63fd847d69d3538552ee10b9a24d5ea790eaf78a
Topper Bowers <topper@toppingdesign.com>
2024-03-27 08:45:51 +0100

    move template into the soul-engine itself (#171)
    * add the template directly here
    
    * copy the template from the repo instead of using git
    
    * play with the compressor
    
    * template again

---
+++ packages/soul-engine-cli/template/soul/initialProcess.ts
@@ -0,0 +1,25 @@
+import { externalDialog, mentalQuery } from "./lib/cognitiveSteps.js";
+import { MentalProcess, useActions, useProcessManager } from "@opensouls/engine";
+import shouts from "./mentalProcesses/shouts.js";
+
+const gainsTrustWithTheUser: MentalProcess = async ({ workingMemory }) => {
+  const { speak, log  } = useActions()
+  const { setNextProcess } = useProcessManager()
+
+  const [withDialog, stream] = await externalDialog(
+    workingMemory,
+    "Talk to the user trying to gain trust and learn about their inner world.",
+    { stream: true, model: "quality" }
+  );
+  speak(stream);
+
+  const [,shouldShout] = await mentalQuery(withDialog, "The interlocuter is being rude?")
+  log("User attacked soul?", shouldShout)
+  if (shouldShout) {
+    setNextProcess(shouts)
+  }
+
+  return withDialog;
+}
+
+export default gainsTrustWithTheUser
---
+++ packages/soul-engine-cli/template/soul/mentalProcesses/shouts.ts
@@ -0,0 +1,36 @@
+import { MentalProcess, indentNicely, useActions, useProcessManager } from "@opensouls/engine";
+import initialProcess from "../initialProcess.js";
+import { externalDialog, mentalQuery } from "../steps/cognitiveSteps.js";
+
+const shouts: MentalProcess = async ({ workingMemory }) => {
+  const { speak, log } = useActions()
+  const { setNextProcess } = useProcessManager()
+
+  const [withYelling, stream] = await externalDialog(
+    workingMemory,
+    indentNicely`
+      - Respond in ALL CAPS
+      - Use capital letters only
+      - Be angry
+      - Be funny
+    `,
+    { stream: true, model: "quality" }
+  );
+
+  speak(stream);
+
+  const [, shouldChill] = await mentalQuery(withYelling, "The interlocuter apologized.")
+
+  log("User apologized?", shouldChill)
+  if (shouldChill) {
+    setNextProcess(initialProcess)
+
+    return withYelling.withMonologue(indentNicely`
+      ${workingMemory.soulName} thought to themself: I need to chill and stop shouting. I will stop using all caps.
+    `)
+  }
+
+  return withYelling
+}
+
+export default shouts
