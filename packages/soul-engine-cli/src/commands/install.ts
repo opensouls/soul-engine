5093631dee1b8e239dbc28e5e8ccf6997fbb9544
Topper Bowers <topper@toppingdesign.com>
2024-03-27 11:21:58 +0100

    support windoze --oops

--- packages/soul-engine-cli/src/commands/install.ts
+++ packages/soul-engine-cli/src/commands/install.ts
@@ -1,14 +1,14 @@
 import { Command } from 'commander'
 import fsExtra from 'fs-extra/esm'
 import { writeFile } from "node:fs/promises"
-import { basename } from "node:path"
+import { basename, join } from "node:path"
 
 const COMMUNITY_ROOT = "https://raw.githubusercontent.com/opensouls/souls/feature/community/library/"
 
 const createInstall = (program: Command) => {
   program
     .command('install')
-    .argument('<packagePath>', 'The full path of the library package (eg cognitiveSteps/externalDialog)')
+    .argument('<packagePath>', 'The full path of the library package (eg cognitiveStep/externalDialog)')
     .description('install a community package from the Open Souls community library')
     .action(async (packagePath: string) => {
       if (!packagePath.endsWith(".ts")) {
@@ -20,11 +20,11 @@ const createInstall = (program: Command) => {
         console.error("Failed to fetch package", packagePath)
         throw new Error("Failed to fetch package: " + packagePath)
       }
-      await fsExtra.mkdirp("soul/lib")
+      await fsExtra.mkdirp(join("soul", "lib"))
 
       const filename = basename(packagePath)
       const data = await resp.text();
-      const destinationPath = `soul/lib/${filename}`
+      const destinationPath = join("soul", "lib", filename)
       await writeFile(destinationPath, data);
       console.log(`${packagePath} has been installed successfully to ${destinationPath}`);
     })
