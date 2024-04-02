5a4fc0b3843ab948054d0e089e15e8401a8ea269
Kevin Fischer <fischer.kevin.a@gmail.com>
2024-04-02 14:34:31 -0700

    Delete core directory (#186)
    Weeeeee - deprecated. goodbye

--- core/tests/shared/dotenv.ts
+++
@@ -1,6 +0,0 @@
-import { SpanProcessorType, startInstrumentation } from '../../src/instrumentation';
-import dotenv from 'dotenv';
-dotenv.config();
-startInstrumentation({
-  spanProcessorType: SpanProcessorType.Simple,
-})
