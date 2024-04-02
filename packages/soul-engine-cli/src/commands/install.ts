12520ee5e61505cab3b7d2fb095296a18b35dac4
Topper Bowers <topper@toppingdesign.com>
2024-04-02 13:45:50 +0200

    import subprocesses and put them in the right place, allow adding multiple files at once

--- packages/soul-engine-cli/src/commands/install.ts
+++ packages/soul-engine-cli/src/commands/install.ts
@@ -5,21 +5,23 @@ import { CommunityInstaller } from '../communityInstaller.js'
 const createInstall = (program: Command) => {
   program
     .command('install')
-    .argument('<packagePath>', 'The full path of the library package (eg cognitiveStep/externalDialog)')
+    .argument('<packagePath...>', 'The full path of the library package (eg cognitiveStep/externalDialog). You can specify multiple library packages.')
     .option('-b,--branch <branch>', 'The branch to install from', "main")
     .description('install a community package from the Open Souls community library')
-    .action(async (packagePath: string, { branch }: { branch: string }) => {
-      if (!packagePath.endsWith(".ts")) {
-        packagePath = packagePath + ".ts"
-      }
+    .action(async (packagePaths: string[], { branch }: { branch: string }) => {
+      for (let packagePath of packagePaths) {
+        if (!packagePath.endsWith(".ts")) {
+          packagePath = packagePath + ".ts"
+        }
 
-      if (! (await fsExtra.pathExists("soul"))) {
-        console.error("You must be in the root of a soul project to install a community file.")
-        return
+        if (!(await fsExtra.pathExists("soul"))) {
+          console.error("You must be in the root of a soul project to install a community package.")
+          return
+        }
+
+        const installer = new CommunityInstaller(packagePath, branch)
+        await installer.install()
       }
-      
-      const installer = new CommunityInstaller(packagePath, branch)
-      await installer.install()
     })
 }
 
