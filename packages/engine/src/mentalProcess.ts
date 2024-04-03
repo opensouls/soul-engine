fc4c28d7c68630168f0695950b73b0339c713703
Topper Bowers <topper@toppingdesign.com>
2024-04-03 21:54:14 +0200

    new process return type OPE-324 (#190)
    * add new mental process return types
    
    * optional params

--- packages/engine/src/mentalProcess.ts
+++ packages/engine/src/mentalProcess.ts
@@ -6,6 +6,13 @@ export interface MentalProcessArguments<ParamType, CortexStepType = any> {
   workingMemory: WorkingMemory
 }
 
+export interface MentalProcessReturnOptions<ParamType> {
+  params?: ParamType,
+  executeNow?: boolean
+}
+
+export type MentalProcessReturnTypes<CortexStepType, ParamType = any> = CortexStepType | WorkingMemory | [WorkingMemory, MentalProcess<ParamType>] | [WorkingMemory, MentalProcess<ParamType>, MentalProcessReturnOptions<ParamType>]
+
 export type MentalProcess<ParamType = Record<number | string, any>, CortexStepType = any> = 
-  (args: MentalProcessArguments<ParamType, CortexStepType>) => Promise<CortexStepType | WorkingMemory>
+  (args: MentalProcessArguments<ParamType, CortexStepType>) => Promise<MentalProcessReturnTypes<CortexStepType, ParamType>>
   
