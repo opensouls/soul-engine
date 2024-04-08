d0c1dde448b93a79d88db91ce7a003753a0c4d04
Topper Bowers <topper@toppingdesign.com>
2024-04-08 17:06:19 +0200

    commands to push/pull blueprint and organization stores

--- packages/soul-engine-cli/src/commands/stores/index.ts
+++ packages/soul-engine-cli/src/commands/stores/index.ts
@@ -1,11 +1,13 @@
 import { Command } from "commander";
 import createStoresPullCommand from "./pull.js";
+import createStoresPushCommand from "./push.js";
 
 
 const createStoreCommand = (program: Command) => {
   const subCommand = program.command('stores')
   createStoresPullCommand(subCommand)
-
+  createStoresPushCommand(subCommand)
+  
   return program
 }
 
