bb6c08fe358c85b581629787552c367bfb87cc35
Topper Bowers <topper@toppingdesign.com>
2024-04-25 14:55:15 +0200

    rename class

--- packages/soul-engine-cli/src/config.ts
+++ packages/soul-engine-cli/src/config.ts
@@ -4,7 +4,11 @@ export interface GlobalConfig {
   organization: string
 }
 
-class ApiConfig {
+/**
+ * This class is designed to provide a consistent API similar to the 'conf' package,
+ * but it specifically handles configuration set directly (in this case environment variables)
+ */
+class EnvironmentVariableConfig {
   constructor(private config: GlobalConfig) {}
 
   get(key: keyof GlobalConfig) {
@@ -21,14 +25,14 @@ export const getConfig = async (isLocal = false) => {
   if (process.env.SOUL_ENGINE_CONFIG) {
     console.log("login with config")
     const parsed = JSON.parse(Buffer.from(process.env.SOUL_ENGINE_CONFIG, "base64").toString("utf8"))
-    return new ApiConfig({
+    return new EnvironmentVariableConfig({
       apiKey: parsed.apiKey,
       organization: parsed.organization.slug,
     })
   }
 
   if (process.env.SOUL_ENGINE_API_KEY && process.env.SOUL_ENGINE_ORGANIZATION) {
-    return new ApiConfig({
+    return new EnvironmentVariableConfig({
       apiKey: process.env.SOUL_ENGINE_API_KEY,
       organization: process.env.SOUL_ENGINE_ORGANIZATION,
     })
