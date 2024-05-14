dd23d7f65ce507bf6c854f2f756bc22d3a288a2d
Topper Bowers <topper@toppingdesign.com>
2024-05-14 12:32:30 +0200

    add a tool calling integration test

--- packages/soul/tests/shared/integrator-test-soul/soul/initialProcess.ts
+++ packages/soul/tests/shared/integrator-test-soul/soul/initialProcess.ts
@@ -1,18 +1,26 @@
 
-import { MentalProcess, useActions, usePerceptions } from "@opensouls/engine";
+import { MentalProcess, useActions, usePerceptions, useTool } from "@opensouls/engine";
 
 const gainsTrustWithTheUser: MentalProcess = async ({ workingMemory }) => {
   const { speak  } = useActions()
   const { invokingPerception } = usePerceptions()
+  const pingTool = useTool<{ping: string}, { pong: string }>("pingTool")
 
-  if (invokingPerception) {
-    speak("You said: " + invokingPerception.content)
-    return workingMemory.withMonologue(`${workingMemory.soulName} said: You said: ${invokingPerception.content}`)
+  if (!invokingPerception) {
+    speak("you said nothing")
+
+    return workingMemory.withMonologue(`${workingMemory.soulName} said: you said nothing`);
   }
 
-  speak("you said nothing")
+  if (invokingPerception.action === "callTool") {
+    const { pong } = await pingTool({ ping: invokingPerception.content })
+    speak("Your tool ponged: " + pong)
+    return workingMemory.withMonologue(`${workingMemory.soulName} said: Your tool ponged: ${pong}`)
+  }
 
-  return workingMemory.withMonologue(`${workingMemory.soulName} said: you said nothing`);
+  speak("You said: " + invokingPerception.content)
+  return workingMemory.withMonologue(`${workingMemory.soulName} said: You said: ${invokingPerception.content}`)
+
 }
 
 export default gainsTrustWithTheUser
--- packages/soul/tests/toolHandlerIntegration.spec.ts
+++ packages/soul/tests/toolHandlerIntegration.spec.ts
@@ -1,8 +1,38 @@
 import { describe } from "mocha";
+import { expect } from "chai";
 import { Soul } from "../src/soul.js";
 
 describe("ToolHandler Integration Tests", () => {
-  const soul = new Soul()
+  before(() => {
+    console.log("make sure you have run npx soul-engine dev -l in the tests/shared/integrator-test-soul directory")
+  })
 
+  it("supplies tools to the soul", async () => {
+    const soul = new Soul({
+      blueprint: "integrator-test-soul",
+      organization: process.env.SOUL_ENGINE_LOCAL_ORGANIZATION!,
+      token: process.env.SOUL_ENGINE_LOCAL_API_KEY!,
+      debug: true,
+      local: true,
+    })
+
+    soul.registerTool<{ ping: string }, { pong: string }>("pingTool", async ({ ping }) => {
+      return { pong: ping }
+    })
+
+    await soul.connect()
+
+    await soul.dispatch({
+      action: "callTool",
+      content: "ping"
+    })
+
+    // wait like 500ms, then check to make sure it ponged.
+    await new Promise((resolve) => setTimeout(resolve, 500))
+    const said = soul.events.find((event) => event.action === "says")
+    expect(said).to.exist
+
+    expect(said?.content).to.equal("Your tool ponged: ping")
+  })
 
 })
