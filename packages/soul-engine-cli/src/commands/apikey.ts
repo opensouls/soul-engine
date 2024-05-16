4b618587fd7fc9148914eb175b72852299734e19
Topper Bowers <topper@toppingdesign.com>
2024-05-16 09:04:28 +0200

    allow json output from apikey and also add organization output (#231)
    * allow json output from apikey and also add organization output, useful for setup scripts
    
    * v0.1.29-alpha.0

--- packages/soul-engine-cli/src/commands/apikey.ts
+++ packages/soul-engine-cli/src/commands/apikey.ts
@@ -6,9 +6,21 @@ const createApiKeyCommand = (program: Command) => {
     .command('apikey')
     .description('print your api key to the terminal. This command is useful for connecting to a debug chat.')
     .option('-l, --local', 'use the local config file', false)
-    .action(async (options: { local: boolean }) => {
+    .option("--json", "output the api key and organization as json", false)
+    .action(async (options: { local: boolean, json: boolean }) => {
       const globalConfig = await getConfig(options.local)
-      console.log("API KEY:", globalConfig.get("apiKey"))
+      const org = globalConfig.get("organization")
+      const apiKey = globalConfig.get("apiKey")
+      
+      if (options.json) {
+        console.log(JSON.stringify({
+          apiKey: apiKey,
+          organization: org
+        }))
+        return
+      }
+      console.log("API KEY:", apiKey)
+      console.log("ORGANIZATION: ", org)
     })
 }
 
